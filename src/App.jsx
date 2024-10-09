ac06f8b3e933212bb32e58cd68cdfe8ca1cc89f3
Utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-10-09 23:42:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { jsQuizz } from './constants'
+import Quiz from './Quiz'
+
+function App() {
+
+  return (
+    <>
+      <Quiz questions={jsQuizz.questions} />
+    </>
+  )
+}
+
+export default App
